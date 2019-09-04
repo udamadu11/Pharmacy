cdd69d680cfa772c1fb9d548a965019dee46a601
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2019-09-04 19:57:04 +0530

    update 2.6

--- php/approval.php
+++ php/approval.php
@@ -2,24 +2,42 @@
 include('include/connection.php');
 if(isset($_POST['approve'])){ 
 
-		$select = "SELECT * FROM tem";
+		$supplier_id = $_POST['approval'];
+		$select = "SELECT * FROM tem WHERE supplier_id = '$supplier_id'";
 		$result1 = $con->query($select);
-			
-		$supplier_id = $_POST['supplier_id'];
-		$supplier_name = $_POST['supplier_name'];
-		$location = $_POST['location'];
-		$email = $_POST['email'];
-		$contact_no = $_POST['contact_no'];
-		$credit_period_allowed = $_POST['credit_period_allowed'];
+		
+		if (mysqli_num_rows($result1) > 0) {
+		while ($row = mysqli_fetch_assoc($result1)) {
+
+		$supplier_id = $row['supplier_id'];
+		$supplier_name = $row['supplier_name'];
+		$location = $row['location'];
+		$email = $row['email'];
+		$contact_no = $row['contact_no'];
+		$credit_period_allowed = $row['credit_period_allowed'];
 
 		$sql = "INSERT INTO supplier (supplier_id,supplier_name,location,email,contact_no,credit_period_allowed) VALUES ('$supplier_id','$supplier_name','$location','$email','$contact_no','$credit_period_allowed')";
 		$result = mysqli_query($con, $sql);
 
-
-		$delete_query ="DELETE FROM tem WHERE supplier_id = '$s_id' ";
+		
+		$delete_query ="DELETE FROM tem WHERE supplier_id = '$supplier_id' ";
 		$delete_result = mysqli_query($con,$delete_query);
-
-
+		$massage = base64_encode(urlencode("Confirmation Successfully"));
+		header('Location:approvalList.php?msg=' .$massage);
+		exit();
 	}
+	}
+}
+
+	if (isset($_POST['decline'])) {
+	$s_id = $_POST['approval2'];
+	$delete_query ="DELETE FROM tem WHERE supplier_id = '$s_id' ";
+	$delete_result = mysqli_query($con,$delete_query);
+
+	$massage = base64_encode(urlencode("Decline Successfully"));
+	header('Location:approvalList.php?msg=' .$massage);
+	exit();
+	}
+
 
 ?>
--- php/approvalList.php
+++ php/approvalList.php
@@ -6,6 +6,7 @@
 	<link rel="stylesheet" type="text/css" href="../css/approvalList.css">
 </head>
 <body>
+	<h3 class="error-msg"><?php include('include/message.php'); ?>
 	<table>
 		<tr>
 			<th>Supplier Id</th>
@@ -37,7 +38,7 @@
 
 					</td>
 					<td>
-								<form method=\"post\" class=\"approval\">
+								<form method=\"post\" class=\"approval\" action=\"approval.php\">
 									<input type=\"hidden\" value=".$row['supplier_id']." name=\"approval2\">
 									<input class=\"btn2\" type=\"submit\" name=\"decline\" value=\"Decline\">
 								</form>
@@ -46,30 +47,13 @@
 				</tr>";
 		}
 	echo "</table";
+	}else{
+		echo "<script>alert('No Pending !')</script>";
 	}
-	else{
-		echo "0 result";
-	}
+	
 	$con->close();
 ?>
 
 	</table>
 </body>
 </html>
-<?php 
-include('include/connection.php');
-	if (isset($_POST['decline'])) {
-	$s_id = $_POST['approval2'];
-	$delete_query ="DELETE FROM tem WHERE supplier_id = '$s_id' ";
-	$delete_result = mysqli_query($con,$delete_query);
-
-	if ($delete_result) {
-		echo "<script>alert('Decline Suceefully')</script>";
-	}
-	else{
-		echo "<script>alert('Decline y')</script>";
-	}
-	}
-	mysqli_close($con);
-
-?>
