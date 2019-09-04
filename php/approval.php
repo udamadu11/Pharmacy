1b3d6679910709f4aa802caa96a6f66cf9e6a3c7
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2019-09-04 10:30:47 +0530

    update 2.5

--- php/approval.php
+++ php/approval.php
@@ -1,5 +1,10 @@
 <?php 
-if(isset($_POST['submit'])){ 
+include('include/connection.php');
+if(isset($_POST['approve'])){ 
+
+		$select = "SELECT * FROM tem";
+		$result1 = $con->query($select);
+			
 		$supplier_id = $_POST['supplier_id'];
 		$supplier_name = $_POST['supplier_name'];
 		$location = $_POST['location'];
@@ -9,6 +14,12 @@ if(isset($_POST['submit'])){
 
 		$sql = "INSERT INTO supplier (supplier_id,supplier_name,location,email,contact_no,credit_period_allowed) VALUES ('$supplier_id','$supplier_name','$location','$email','$contact_no','$credit_period_allowed')";
 		$result = mysqli_query($con, $sql);
+
+
+		$delete_query ="DELETE FROM tem WHERE supplier_id = '$s_id' ";
+		$delete_result = mysqli_query($con,$delete_query);
+
+
 	}
 
 ?>
