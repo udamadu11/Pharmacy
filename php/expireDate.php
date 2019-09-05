d389f0d8c0b52a960f69f1561703ba67ca384420
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2019-09-05 10:20:03 +0530

    update 3.1

--- php/expireDate.php
+++ php/expireDate.php
@@ -1,22 +1,26 @@
 <?php 
+include('include/connection.php');
+$query = "SELECT * FROM batch";
+$query = mysqli_query($con,$query);
 
 
-$exp_date = "2019/09/4";
-$today = date('Y-m-d H:i:s');
-$exp =strtotime($exp_date);
-$td = strtotime($today);
+while($row = mysqli_fetch_assoc($query)){
+		$exp_date = $row['ex_date'];
+		$today = date('Y-m-d H:i:s');
+		$exp =strtotime($exp_date);
+		$td = strtotime($today);
 
-if ($td>$exp) {
-	$diff = $td - $exp;
-	$x =abs(floor($diff/ (60 * 60 *24)));
-	echo "expire";
-	echo "expireed :".$x;
-}else{
-	$diff = $td - $exp;
-	$x =abs(floor($diff/ (60 * 60 *24)));
-	echo "not expire";
-	echo "expire remininig :".$x;
+		if ($td>$exp) {
+			$diff = $td - $exp;
+			$x =abs(floor($diff/ (60 * 60 *24)));
+			echo "expire";
+			echo "expireed :".$x;
+		}else{
+			$diff = $td - $exp;
+			$x =abs(floor($diff/ (60 * 60 *24)));
+			echo "not expire";
+			echo "expire remininig :".$x;
+		}
+
 }
-	
-
 ?>
