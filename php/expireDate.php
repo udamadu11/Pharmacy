ca8402ce7717529505894713d9ee3a5e90328138
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2019-09-05 10:38:57 +0530

    update 3.3

--- php/expireDate.php
+++ php/expireDate.php
@@ -6,7 +6,7 @@ $query = mysqli_query($con,$query);
 
 while($row = mysqli_fetch_assoc($query)){
 		$exp_date = $row['ex_date'];
-		$today = date('Y-m-d H:i:s');
+		$today = date('Y-m-d');
 		$exp =strtotime($exp_date);
 		$td = strtotime($today);
 
@@ -14,6 +14,43 @@ while($row = mysqli_fetch_assoc($query)){
 		$seconds_diff = $warning_days * 24 * 3600;
 		$warning_timestamp = $exp - $seconds_diff;
 		$warning_date = date('Y-m-d', $warning_timestamp);
-		echo $warning_date;
+		
+		if ($warning_date == $today) {
+			echo "
+			<table>
+			<tr>
+					<th>Batch No</th>
+					<th>Drug Name</th>
+					<th>Brand</th>
+					<th>Quantity</th>
+					<th>Box</th>
+					<th>Manufacuring Date</th>
+					<th>Expiry Date</th>
+					<th>value</th>
+					<th>Supplier Id</th>
+					<th>Send Email</th>
+			</tr>
+
+			<tr>
+									<td>".$row['batch_no']."</td>
+									<td>".$row['drug_name']."</td>
+									<td>".$row['brand']."</td>
+									<td>".$row['quantity_box']."</td>
+									<td>".$row['no_of_boxes']."</td>
+									<td>".$row['manu_date']."</td>
+									<td>".$row['ex_date']."</td>
+									<td>".$row['sales_value']."</td>
+									<td>".$row['supplier_id']."</td>
+									<td>
+										<form method=\"post\" class=\"send\">
+										<input type=\"hidden\" value=".$row['batch_no']." name=\"send\">
+										<input class=\"btn\" type=\"submit\" name=\"submit\" value=\"Send\">
+										</form>
+									</td>
+			</tr>
+			</table>
+			";
+			
+		}
 }
 ?>
