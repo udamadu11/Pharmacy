f708007847b76543e7c91b87563288c35e194cb0
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2019-09-08 10:30:30 +0530

    update 3.7

--- php/creditPeriod.php
+++ php/creditPeriod.php
@@ -4,18 +4,12 @@ include('include/connection.php');
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Credi period notification</title>
+	<title>Credit period notification</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
 
-	<table>
-			<tr>
-					<th>Batch No</th>
-					<th>Drug Name</th>
-					<th>Brand</th>
-					
-					
-			</tr>
+	
 <?php 
 include('include/connection.php');
 $query = "SELECT * FROM broughtin";
@@ -28,26 +22,75 @@ while($row = mysqli_fetch_assoc($result)){
 		$exp =strtotime($exp_date);
 		$td = strtotime($today);
 
-		$warning_days = 22;
+		$warning_days = 24;
 		$seconds_diff = $warning_days * 24 * 3600;
 		$warning_timestamp = $exp - $seconds_diff;
 		$warning_date = date('Y-m-d', $warning_timestamp);
 		
-		if ($warning_date == $today) {
+		if ($warning_date <= $today) {
+
+			$invoice_no = $row['invoice_no'];
+			$supplier_id = $row['supplier_id'];
+			$credit_period = $row['credit_period'];
 			echo "
-			<tr>
-				<td>".$row['invoice_no']."</td>
-				<td>".$row['date']."</td>
-				<td>".$row['supplier_id']."</td>
-								
-			</tr>
-			</table>
+			
+
+	<div class=\"card\" style=\"width:18rem; float:left;margin-left:80px;margin-top:40px;\">
+  <div class=\"card-header\" style=\"background-color:skyblue;\">
+    Credit Period Notification
+  </div>
+  <ul class=\"list-group list-group-flush\">
+    <li class=\"list-group-item\"> invoice No : $invoice_no</li>
+    <li class=\"list-group-item\"> supplier Id : $supplier_id</li>
+    <li class=\"list-group-item\"> credit Period : $credit_period</li>
+    <li class=\"list-group-item\">
+    <form method=\"post\" class=\"dl\">
+		<input type=\"hidden\" value=\"$supplier_id\" name=\"send\">
+		<input class=\"btn btn-info\" type=\"submit\" name=\"submit\" value=\"Send Email\" style =\"margin-left:50px;\">
+	</form></li>
+   
+  </ul>
+</div>
+
 			";
 		}
 			
 		}
 
+if(isset($_POST['submit'])){ 
 
+		$supplier_id = $_POST['send'];
+		$select = "SELECT * FROM supplier WHERE supplier_id = '$supplier_id'";
+		$result = $con->query($select);
+		if (mysqli_num_rows($result) > 0) {
+		while ($row = mysqli_fetch_assoc($result)) {
+			$email = $row['email'];
+
+			
+			$to = $email;
+			$subject = "Notification of PHARMA-PRO About Credit Period";
+			$message = "Your Credit .......";
+
+			$headers = "MIME-Version: 1.0" . "\r\n";
+			$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+			$headers .= 'From: <udaramadumalka3@gmail.com>' . "\r\n";
+			$mail = mail($to,$subject,$message,$headers);
+			if ($mail) {
+				echo "<script>alert('Thank You..!..We have sent an email to the Supplier.')</script>";
+			}
+			else{
+				echo "<script>alert('Error.')</script>";
+			}
+
+
+
+
+		
+	}
+}
+}
+		
 ?>
 
 
