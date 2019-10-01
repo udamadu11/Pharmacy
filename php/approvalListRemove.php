794a5f536e1cfa40248465195a7ca1c11e3c957d
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2019-10-01 08:24:14 +0530

    update 5.2

--- php/approvalListRemove.php
+++ php/approvalListRemove.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Approval list</title>
+	<title>Approval list..</title>
 	<link rel="stylesheet" type="text/css" href="../css/approvalList.css">
 </head>
 <body>
