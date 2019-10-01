d2043b4595df50ea378597d4199849f469b5a595
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2019-10-01 23:01:03 +0530

    update 5.5

--- php/addPurchase.php
+++ php/addPurchase.php
@@ -3,11 +3,11 @@
 	if(isset($_POST['submit'])){ 
 
 		$supplier_id=$_POST['supplier_id'];
-		$date=$_POST['date'];
+		$date= date("Y-m-d H:i:s");
 		$invoice=$_POST['invoice'];
 		
 
-		$sql = "INSERT INTO drug (supplier_id,date,invoice) VALUES ('$supplier_id','$date','$invoice')";
+		$sql = "INSERT INTO purchase (supplier_id,date,invoice) VALUES ('$supplier_id','$date','$invoice')";
 		mysqli_query($con, $sql);
 	}
 ?>
@@ -16,20 +16,34 @@
 <html>
 <head>
 	<title>Add Purchase</title>
-	<link rel="stylesheet" type="text/css" href="../css/addDrug.css">
+	<link rel="stylesheet" type="text/css" href="../css/addPurchase.css">
 </head>
 <body>
-	<form class="addDrug" method="post">
-		<img src="../img/pharmacist.png">
-		<h2>Add Drug</h2>
+	<form class="addPurchase" method="post">
+		<img src="../img/shopping-cart.png">
+		<h2>Add Purchase</h2>
 		<div class="input_fields">
 				
-				<p>Drug Id</p>
-				<input type="number" class="input" name="drug_id" id="drug_id" placeholder="Enter Drug Id">
-
+				<p>Supplier Id</p>
+				<select class="form-control" name="supplier_id">
+						<?php
+							include ('include/connection.php');
+							$sqlSupplier = "SELECT * FROM supplier";
+							$resultSupplier = mysqli_query($con,$sqlSupplier);
+							while ($row = mysqli_fetch_array($resultSupplier)) {
+							$sName = $row['supplier_name'];
+							$sid = $row['supplier_id'];
+							echo '<option value="'.$sid.'">'.$sName.'</option>';
+						}
+					?>
+						
+				</select>
+				
+				<p>Price</p>
+				<input type="text" class="input" name="invoice" id="invoice" placeholder="Enter the Invoice price">
 				
 		</div>
-		<input type="submit" name="submit" value="Add Drug">
+		<input type="submit" name="submit" value="Add Purchase">
 	</form>
 </body>
 </html>
