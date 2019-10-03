778d5b1692c67ea217a37a9463c19f2d4f8b4cd7
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2019-10-03 23:22:36 +0530

    update 1.8.5

--- php/viewEmployee.php
+++ php/viewEmployee.php
@@ -3,11 +3,12 @@
 <html>
 <head>
 	<title>View Employee</title>
-	<link rel="stylesheet" type="text/css" href="../css/viewEmployee.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-<table>
-	<tr>
+	<div class="container" style="margin-top: 50px;">
+		<table class="table table-hover">
+	<tr class="table-info">
 		<th>Id</th>
 		<th>First Name</th>
 		<th>Last Name</th>
@@ -43,5 +44,7 @@
 
 	?>
 </table>
+	</div>
+
 </body>
 </html>
