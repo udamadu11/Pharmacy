b2112c09fbf98f6da8d88b0f88a908180c220ff1
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2019-10-19 22:10:28 +0530

    update 1.3.1

--- php/approvalList.php
+++ php/approvalList.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Approval list</title>
+	<title>Approval lists</title>
 	<link rel="stylesheet" type="text/css" href="../css/approvalList.css">
 </head>
 <body>
