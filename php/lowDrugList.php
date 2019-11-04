3b2d8fb8bc2d3e289aa0a3c65c603f0d41b1e89b
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2019-11-04 08:50:14 +0530

    update 1.4.6

--- php/lowDrugList.php
+++ php/lowDrugList.php
@@ -6,11 +6,31 @@
 </head>
 <body>
 <?php 
-	$sql = "SELECT * FROM batch";
-	$result = mysqli_query($con,$sql);
+	$sql = "SELECT drug.drug_id,drug.drug_name,drug.reorder_level, stock.current_stock FROM drug INNER JOIN stock ON drug.drug_id = stock.drug_id";
+	$query= mysqli_query($con,$sql);
+	echo "
+				<table>
+							<tr>
+									<th>Drug Id</th>
+									<th>Drug Name</th>
+									<th>Reoder Level</th>
+									<th>Current Stock</th>
+							</tr>
 
-	
-
+	";
+	if (mysqli_num_rows($query) > 0) {
+			while($row = mysqli_fetch_assoc($query)){
+							echo "
+						
+							<tr>
+									<td>".$row['drug_id']."</td>
+									<td>".$row['drug_name']."</td>
+									<td>".$row['reorder_level']."</td>
+									<td>".$row['current_stock']."</td>
+							</tr>";
+						}
+				}	
 ?>
+</table>
 </body>
 </html>
--- php/viewUser.php
+++ php/viewUser.php
@@ -13,11 +13,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="../css/viewUser.css">
+	<title>View Users</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-	<table>
+	<div class="container" style="margin-top: 100px;">
+	<table class="table table-hover">
 		<tr>
 			<th>Id</th>
 			<th>F_Name</th>
@@ -57,6 +58,6 @@
 		?>
 
 	</table>
-
+</div>
 </body>
 </html>
