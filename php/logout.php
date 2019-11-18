fdbd1f55f12aed80f36dc5ad2fffde8ee5eed2c3
iromiesamarasekara <38650079+iromiesamarasekara@users.noreply.github.com>
2019-11-18 18:07:23 +0530

    Add files via upload

--- php/logout.php
+++ php/logout.php
@@ -1,9 +1,8 @@
 <?php
-	require_once('include/connection.php');//include databse connection
+	require_once('include/connection.php');
 
 	session_start();
 	if(isset($_SESSION['u_name'])){
-		// remove  session variables
 		session_unset($_SESSION['u_name']);
 		session_unset($_SESSION['type']);
 		session_unset($_SESSION['id']);
